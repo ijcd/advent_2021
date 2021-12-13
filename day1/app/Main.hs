dbe8dd7b9c6b91f2b8ab42b3db13f9b0f9eab7e5
Ian Duggan <ian@ianduggan.net>
2021-12-12 20:37:41 -0800

    Day1 complete

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import Data.List
+
+readInts :: Read b => FilePath -> IO [b]
+readInts fname = do
+  contents <- readFile fname
+  let asLines = lines contents
+  let numbers = map read asLines
+  return numbers
+
+windows :: Int -> [a] -> [[a]]
+windows n = transpose . take n . tails
+
+findIncreases1 depths =
+  length [(a, b) | (a, b) <- zip depths (tail depths), b > a]
+
+findIncreases3 depths =
+  length [(sum a, sum b) | (a, b) <- zip wins (tail wins), sum b > sum a]
+  where
+    wins = windows 3 depths
 
 main :: IO ()
-main = someFunc
+main = do
+  depths <- readInts "input.txt" :: IO [Int]
+  print $ findIncreases1 depths
+  print $ findIncreases3 depths
